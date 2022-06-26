026251bf9ac6bc5fd54f7bf8a1deb0ef867405d1
Bruno Caruso <brunofcaruso@gmail.com>
2022-06-26 20:45:09 -0300

    Hero modifications

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,20 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Flex, Text, Heading } from "@chakra-ui/react";
+import { Container } from "./Container";
 
 export const Hero = ({ title }: { title: string }) => (
-  <Flex
-    justifyContent="center"
-    alignItems="center"
-    height="100vh"
-    bgGradient="linear(to-l, heroGradientStart, heroGradientEnd)"
-    bgClip="text"
-  >
-    <Heading fontSize="6vw">{title}</Heading>
-  </Flex>
+  <Container justifyContent="center" alignItems="center" height="50vh">
+    <Flex
+      bgGradient="linear(to-l, heroGradientStart, heroGradientEnd)"
+      bgClip="text"
+    >
+      <Heading fontSize="6vw">Welcome!</Heading>
+    </Flex>
+    <Text textAlign={"center"}>
+      Hello, i am Bruno, a passionate Full Stack Developer & technology
+      enthusiast. I have a high proficiency in JavaScript and a user-centered
+      mentality for building products. I also like finances, fitness,
+      hackintoshing and entrepreneurial spirit. Self-taught by nature and
+      constantly learning.
+    </Text>
+  </Container>
 );
-
-Hero.defaultProps = {
-  title: "with-chakra-ui-typescript",
-};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,12 @@ import { Container } from "../components/Container";
 import { CTA } from "../components/CTA";
 import { Footer } from "../components/Footer";
 import NavBar from "../components/NavBar/NavBar";
+import { Hero } from "../components/Hero";
 
 const Index = () => (
   <>
     <NavBar />
+    <Hero />
     <Container height="100vh">
       <Footer>
         <Text>Next ❤️ Chakra</Text>
