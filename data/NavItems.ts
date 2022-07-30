8993d0c9433a7b6e5425887314d805801d1b1bf4
Bruno Caruso <brunofcaruso@gmail.com>
2022-07-30 00:47:10 -0300

    Minor data change on NavItem

--- data/NavItems.ts
+++ data/NavItems.ts
@@ -9,13 +9,13 @@ export const NAV_ITEMS: Array<NavItem> = [
     label: "Projects",
     children: [
       {
-        label: "As employee",
+        label: "Work",
         subLabel: "Projects done on my daily work",
         href: "#",
       },
       {
-        label: "Freelance",
-        subLabel: "Contract work projects",
+        label: "Personal",
+        subLabel: "Own personal projects",
         href: "#",
       },
     ],
