7fab4883b491e6968417f8546123d405fe1b88cc
Bruno Caruso <brunofcaruso@gmail.com>
2022-08-24 22:55:39 -0300

    fix spacing

--- src/components/Templates/Home/index.tsx
+++ src/components/Templates/Home/index.tsx
@@ -1,13 +1,17 @@
-import { Container } from "@chakra-ui/react";
+import React from "react";
+import { Container, VStack } from "@chakra-ui/react";
 import Hero from "../../Organisms/Hero";
 import { Services } from "../../Organisms/Services";
-import React from "react";
+import { AboutMe } from "../../Organisms/AboutMe";
 
 // TODO: make dark mode automatically with mobile device modes
 
 export const Home = () => (
   <Container maxW={"7xl"}>
     <Hero />
-    <Services />
+    <VStack spacing={10}>
+      <Services />
+      <AboutMe />
+    </VStack>
   </Container>
 );
