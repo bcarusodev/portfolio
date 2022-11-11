e6c63f3f70ca46564fc5406a9244f09b24de2815
Bruno Caruso <brunofcaruso@gmail.com>
2022-11-11 19:12:31 -0300

    Add see more btn in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,30 @@
 import React from "react";
-import { Container, VStack } from "@chakra-ui/react";
+import { Button, Container, VStack } from "@chakra-ui/react";
 import { AboutMe } from "../components/Organisms/AboutMe";
 import Hero from "../components/Organisms/Hero";
 import { Services } from "../components/Organisms/Services";
+import { useRouter } from "next/router";
 
-const Index = () => (
-  <>
-    <Container maxW={"7xl"}>
-      <Hero />
-      <VStack spacing={10}>
-        <Services />
-        <AboutMe />
-      </VStack>
-    </Container>
-  </>
-);
+const Index = () => {
+  const router = useRouter();
+  return (
+    <>
+      <Container maxW={"7xl"}>
+        <Hero />
+        <VStack spacing={10}>
+          <Services />
+          <AboutMe />
+          <Button
+            size={"lg"}
+            fontWeight={"normal"}
+            onClick={() => router.push("/about")}
+          >
+            See more
+          </Button>
+        </VStack>
+      </Container>
+    </>
+  );
+};
 
 export default Index;
