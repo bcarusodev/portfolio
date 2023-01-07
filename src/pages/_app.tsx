4e2745bb32ed7e6d0e0eb3a472c713498117741a
Bruno Caruso <brunofcaruso@gmail.com>
2023-01-07 00:46:11 -0300

    Fix anim duration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import "@fontsource/plus-jakarta-sans/500.css";
 import "@fontsource/plus-jakarta-sans/300.css";
 import "@fontsource/plus-jakarta-sans/600.css";
 import "@fontsource/plus-jakarta-sans/400.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Fade } from "@chakra-ui/react";
 
 import theme from "../theme";
 import { AppProps } from "next/app";
@@ -11,14 +11,20 @@ import Footer from "../components/Organisms/Footer";
 import NavBar from "../components/Organisms/NavBar";
 import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
       <Head>
         <title>Portfolio - Bruno Caruso</title>
       </Head>
       <NavBar />
-      <Component {...pageProps} />
+      <Fade
+        key={router.route}
+        in={true}
+        transition={{ enter: { duration: 0.35 }, exit: { duration: 0.35 } }}
+      >
+        <Component {...pageProps} />
+      </Fade>
       <Footer />
     </ChakraProvider>
   );
